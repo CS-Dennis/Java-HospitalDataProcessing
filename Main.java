975b98a4036637c2ccd9eda910e8d98bf004b611
NanYe <55266315+GIS-Dennis@users.noreply.github.com>
2019-09-22 20:00:47 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         // write your code here
         Long startTime = System.currentTimeMillis();
-        HospitalDataProcessor p1 = new HospitalDataProcessor("C:/Users/Dennis/Desktop/java/files/Hospital_General_Information.txt");
-        p1.displayAndWriteSearchResults("C:/Users/Dennis/Desktop/java/files/new.txt");
+        HospitalDataProcessor p1 = new HospitalDataProcessor("/Users/dennis/Desktop/java/files/Hospital_General_Information.txt");
+        p1.displayAndWriteSearchResults("/Users/dennis/Desktop/java/files/new.txt");
         Long finishTime = System.currentTimeMillis();
 
         System.out.println(finishTime-startTime);
